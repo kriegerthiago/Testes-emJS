c9a1b1ebf2a5f646472782f603b6be0a2b1a0251
Thiago <thiagokriegercardoso@gmail.com>
2021-09-06 12:15:14 -0300

    Testes parte 1

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ let language;
 console.log(typeof isIsland, typeof country,typeof language,typeof population)
 
 // Atividade 3 -
-language = 'portugues'
+language = 'english'
 
 population = 100;
 population++;
@@ -115,7 +115,7 @@ console.log(5 + 6 + '4' + 9 - 4 - 2); // 1143
 
 // Atividade 6 -
 
-let numNeighbors = Number(prompt("How many neighbors does your country have?"));
+/*let numNeighbors = Number(prompt("How many neighbors does your country have?"));
 
 if (numNeighbors === 1) {
     console.log('Only 1 border!')
@@ -124,6 +124,73 @@ if (numNeighbors === 1) {
 }else{
     console.log('No borders')
 }
+*/
 
 // Atividade 7 - 
+ //population = 30 // teste
+ 
+if (language == 'english' && population < 50 && isIsland == false) {
+      console.log("You should live in Portugal")  
+}else{
+    console.log("Portugal does not meet your criteria :( ")
+}
 
+
+
+// Atividade 8 -
+language = 'english'
+switch (language) {
+    case 'chinese':
+    case 'mandarin':    
+        console.log("Most numbers of native speakers")
+        break;
+    case 'spanish':
+        console.log("2nd place in native speakers number")
+        break;
+    case 'english':
+        console.log("3rd place")
+        break;
+    case 'hindi':
+        console.log("number 4")
+        break;
+    case 'arabic':
+        console.log("5th most spoken language")
+        break;        
+    default:
+        console.log("Great language too")
+        break;
+}
+
+// Test Dolphins and Koalas
+
+/*
+const averageScoreDolphin = (96+108+89) / 3 ;
+
+const averageScoreKoalas = (88+91+110) / 3 ;
+if (averageScoreDolphin > averageScoreKoalas) {
+    console.log("Dolphin is the winner")
+}else if (averageScoreDolphin === averageScoreKoalas){
+    console.log("ITS A DRAW")
+}else if(averageScoreDolphin < averageScoreKoalas){
+    console.log("Koala is the winner")
+}else if(averageScoreDolphin > 100){
+
+}
+*/
+
+const averageScoreDolphin = (97+112+81) / 3 ;
+
+const averageScoreKoalas = (109+95+86) / 3 ;
+if (averageScoreDolphin > averageScoreKoalas && averageScoreDolphin >= 100) {
+    console.log("Dolphin is the winner")
+}else if (averageScoreDolphin === averageScoreKoalas && averageScoreKoalas >=100 && averageScoreDolphin >=100){
+    console.log("ITS A DRAW")
+}else if(averageScoreDolphin < averageScoreKoalas && averageScoreKoalas >=100){
+    console.log("Koala is the winner")
+}else{
+    console.log("No team wins")
+}
+
+// Atividade 9 - 
+population = 13
+population >=33 ? console.log(`${country}'s population is above average`) : console.log(`${country}'s population is below average`)
